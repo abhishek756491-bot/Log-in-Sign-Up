bae7b3d2f7879e4921a6dcd6a1091548e3c61d30
abhishek kumar <abhishek756491@gmail.com>
2026-06-07 21:59:50 +0530

    update

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const RegisterLink=document.querySelector('.SignUpLink')
 RegisterLink.addEventListener('click',()=>{
     container.classList.add('active');
 })
+LoginLink.addEventListener('click',()=>{
+    container.classList.remove('active');
+})
